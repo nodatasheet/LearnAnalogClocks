00216e34265b6fab26ecffe9b1bbf6489aa7af31
Konstantin <92411255+nodatasheet@users.noreply.github.com>
2025-05-03 19:08:47 +0800

    add _set_colors

--- clocks.py
+++ clocks.py
@@ -18,9 +18,17 @@ class AnalogClock(QWidget):
     def __init__(self, time: QTime, parent=None):
         super().__init__(parent)
         self._time = time
+        self._set_colors()
 
     def set_time(self, time: QTime):
         self._time = time
+
+    def _set_colors(self):
+        self._hour_numbers_color = QColor("red")
+        self._hour_hand_color = QColor("red")
+
+        self._minute_numbers_color = QColor("teal")
+        self._minute_hand_color = QColor("teal")
 
     def paintEvent(self, event):
         side = min(self.width(), self.height())
@@ -40,24 +48,21 @@ class AnalogClock(QWidget):
         self.draw_minute_hand(painter, self._time)
 
     def draw_hour_marks(self, painter: QPainter) -> None:
-        painter.setPen(QColor("white"))
         for i in range(12):
             painter.drawLine(70, 0, 80, 0)
             painter.rotate(30)
 
     def draw_minute_marks(self, painter: QPainter) -> None:
-        self.minute_marks_color = QColor("white")
-        painter.setPen(self.minute_marks_color)
         for i in range(60):
             if i % 5 != 0:
                 painter.drawLine(77, 0, 80, 0)
             painter.rotate(6)
 
     def draw_hour_numbers(self, painter: QPainter) -> None:
-        self.hour_text_color = QColor("red")
-        painter.setPen(self.hour_text_color)
+        painter.setPen(self._hour_numbers_color)
         font = painter.font()
         font.setPointSize(10)
+        font.setBold(True)
         painter.setFont(font)
         circle_diameter = 60
 
@@ -68,8 +73,7 @@ class AnalogClock(QWidget):
             painter.drawText(int(x - 5), int(y + 5), str(i))
 
     def draw_minute_numbers(self, painter: QPainter) -> None:
-        self.minute_text_color = QColor("cyan")
-        painter.setPen(self.minute_text_color)
+        painter.setPen(self._minute_numbers_color)
         font = painter.font()
         font.setPointSize(6)
         font.setBold(True)
@@ -88,14 +92,12 @@ class AnalogClock(QWidget):
                 painter.drawText(int(x + x_offset), int(y + y_offset), str(i))
 
     def draw_hour_hand(self, painter: QPainter, time: QTime) -> None:
-        self.hour_hand_color: QColor = QColor("red")
-
         self.hour_hand = QPolygon([
             QPoint(5, 8),
             QPoint(-5, 8),
             QPoint(0, -35)
         ])
-        painter.setBrush(self.hour_hand_color)
+        painter.setBrush(self._hour_hand_color)
         painter.setPen(Qt.PenStyle.NoPen)
         painter.save()
         hour_angle = 30 * (time.hour() % 12 + time.minute() / 60.0)
@@ -104,14 +106,12 @@ class AnalogClock(QWidget):
         painter.restore()
 
     def draw_minute_hand(self, painter: QPainter, time: QTime) -> None:
-        self.minute_hand_color: QColor = QColor("cyan")
-
         self.minute_hand = QPolygon([
             QPoint(3, 8),
             QPoint(-3, 8),
             QPoint(0, -55)
         ])
-        painter.setBrush(self.minute_hand_color)
+        painter.setBrush(self._minute_hand_color)
         painter.setPen(Qt.PenStyle.NoPen)
         painter.save()
         minute_angle = 6 * (time.minute() + time.second() / 60.0)
@@ -141,10 +141,9 @@ class DigitalClock(QLabel):
         # TODO: use proper formatting and assign color
 
         hours = self._text_style(self._time.toString("hh"), "red")
-        minutes = self._text_style(self._time.toString("mm"), "cyan")
-        separator = self._text_style(":", "white")
+        minutes = self._text_style(self._time.toString("mm"), "teal")
 
-        self.setText(f"{hours}{separator}{minutes}")
+        self.setText(f"{hours}:{minutes}")
 
     def _text_style(self, text: str, color: str) -> str:
         return (
